ad1de324808b6ccd067d5dc63fbea71d0582ade0
RAVIRAJ PRAVIN WAGH <103710873+RAVIRAJWAGH@users.noreply.github.com>
2023-04-17 12:01:59 +0530

    Add files via upload

---
+++ change name of categorical point on x-axis in sns.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # To change name of categorical point on x-axis in sns
+
+# In[ ]:
+
+
+import seaborn as sns
+plt.figure(figsize=(6,6))
+ax=sns.countplot(x= credit_df['Default'])
+for label in ax.containers:
+    ax.bar_label(label)
+plt.xticks([0,1], labels=["Not Deafaulted", "Defaulted"])
+plt.show()
+
+
+# In[ ]:
+
+
+
+
