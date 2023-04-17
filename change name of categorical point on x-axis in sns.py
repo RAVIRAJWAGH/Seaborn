b48c664a2176e01a6384bd945e324a737094a0e8
RAVIRAJ PRAVIN WAGH <103710873+RAVIRAJWAGH@users.noreply.github.com>
2023-04-17 12:12:15 +0530

    Update change name of categorical point on x-axis in sns.py

--- change name of categorical point on x-axis in sns.py
+++ change name of categorical point on x-axis in sns.py
@@ -9,7 +9,7 @@
 import seaborn as sns
 plt.figure(figsize=(6,6))
 ax=sns.countplot(x= credit_df['Default'])
-for label in ax.containers:
+for label in ax.containers: #label on top of the bar
     ax.bar_label(label)
 plt.xticks([0,1], labels=["Not Deafaulted", "Defaulted"])
 plt.show()
